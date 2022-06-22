70e28c0f914470840e7b8fdeb389b7726d80c7ad
prasannarames <prasannaramesh98@gmail.com>
2022-06-22 18:52:53 +0530

    update: added recursion problem

---
+++ Basic-programming-concepts/recursion.js
@@ -0,0 +1,37 @@
+// // recursion
+// -----------------------
+// function recursion(n) {
+//   if(n == 0)
+//     return;
+//   recursion(n-1)
+// }
+// console.log(recursion(5))
+
+
+
+// // print number forward 1 to 10
+// -------------------------
+// function recursion(n) {
+//  // base rule
+//   if(n == 0)
+//     return;
+
+//   //recursive rule
+//   recursion(n-1)
+//   console.log(n)
+// }
+// recursion(10)
+
+
+// // print number backward 10 to 1
+// -------------------------------
+// function recursion(n) {
+//  // base rule
+//   if(n == 0)
+//     return;
+
+//   //recursive rule
+//   console.log(n)
+//   recursion(n-1)
+// }
+// recursion(10)
