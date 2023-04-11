f5805a21d8e51544262a444fb63a84cbfef3c898
prasannarames <prasannaramesh98@gmail.com>
2023-04-11 16:55:28 +0530

    Update script.js

--- DSA_ARRAYS/print_matrix_in_spiral_order/script.js
+++ DSA_ARRAYS/print_matrix_in_spiral_order/script.js
@@ -23,13 +23,70 @@
 // Constraints
 // 1<=A<=1000
 
+// input = 3
+
+// 1 2 3
+// 8 9 4
+// 7 6 5
+
+// input = 4
+
+// 1  2  3  4
+// 12 13 14 5
+// 11 16 15 6
+// 10 9  8  7
+
+// function spiralMatrixII(n) {
+//   let left = 0,
+//     right = n - 1,
+//     top = 0,
+//     bottom = n - 1;
+//   let num = 1;
+//   // create 2d array
+//   let arr = new Array(n);
+//   for (let i = 0; i < n; i++) {
+//     arr[i] = new Array(n);
+//   }
+
+//   while (num <= n * n) {
+//     for (let i = left; i <= right; i++) {
+//       arr[top][i] = num++;
+//     }
+//     top++;
+
+//     for (let i = top; i <= bottom; i++) {
+//       arr[i][right] = num++;
+//     }
+//     right--;
+
+//     for (let i = right; i >= left; i--) {
+//       arr[bottom][i] = num++;
+//     }
+//     bottom--;
+
+//     for (let i = bottom; i >= top; i--) {
+//       arr[i][left] = num++;
+//     }
+//     left++;
+//   }
+//   return arr;
+// }
+
 function spiralMatrixII(n) {
-  let left = 0,
-    right = n - 1,
-    top = 0,
-    bottom = n - 1;
+  // (0,0) (0,1) (0,2) (0,3)
+  // (1,0) (1,1) (1,2) (1,3)
+  // (2,0) (2,1) (2,2) (2,3)
+  // (3,0) (3,1) (3,2) (3,3)
+
+  let left = 0;
+  let right = n - 1;
+  let top = 0;
+  let bottom = n - 1;
+
   let num = 1;
+
   // create 2d array
+
   let arr = new Array(n);
   for (let i = 0; i < n; i++) {
     arr[i] = new Array(n);
@@ -56,6 +113,7 @@ function spiralMatrixII(n) {
     }
     left++;
   }
+
   return arr;
 }
-spiralMatrixII(3);
+console.log(spiralMatrixII(4));
