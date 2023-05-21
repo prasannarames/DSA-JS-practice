e30e30f7e92432bdda22a69f51bbd2751a2003ef
prasannarames <prasannaramesh98@gmail.com>
2023-05-21 12:50:30 +0530

    new problem solution added

--- DSA-constructs-01/Capitalise-first-letter-of-every-word/script.js
+++ DSA-constructs-01/Capitalise-first-letter-of-every-word/script.js
@@ -20,7 +20,7 @@
 
 // The paragraph contains spaces, lowercase, uppercase characters and ‘.’.
 
-// function capitalise(paragraph) {
+function capitalise(paragraph) {
 //   // let ans = "";
 
 //   //   for(let i=0; i<paragraph.length; i++){
@@ -43,68 +43,68 @@
 //   // }
 //   // return para.join(" ");
 
-
-//   // 1. create an empty string to store the capitalized answer.
-//   let ans = "";
-//   // 2. find the difference of ASCII values of the lowerCase and upperCase characters using charCodeAt(0)
-//   let diff = "a".charCodeAt(0) - "A".charCodeAt(0);
-//   // 3. loop through i to paragraph length
-//   for (let i = 0; i < paragraph.length; i++) {
-//     // 3.1. check the first character of the each word.
-//     //       => con-1 is to get the first words first letter  
-//     //      (condition 1 => i == 0 && para[i] >= "a" && para[i]<= "z") ||
-//     //       => con-2 is to get the other words in the paragraph using empty spaces
-//     //      (condition 2 => i > 0 && para[i - 1] == "" && para[i]>="a" && para[i]<="z")  
-//     if (
-//       (i == 0 && paragraph[i] >= "a" && paragraph[i] <= "z") ||
-//       (i > 0 &&
-//         paragraph[i - 1] == " " &&
-//         paragraph[i] >= "a" &&
-//         paragraph[i] <= "z")
-//     ) {
-//       // 3.2. if any one condition is true, get the ascii value of the character - difference and from that value get character accordingly and concatenate with the capitalized answer string.
-//       ans += String.fromCharCode(paragraph[i].charCodeAt(0) - diff);
-//     } else {
-//       // 3.3. if none of the condition is true , return the exact string as concatenated
-//       ans += paragraph[i];
-//     }
-//   }
-//   // 4. finally return capitalised answer string
-//   return ans;
-// }
-
-// console.log(capitalise("the quick Brown fox jumps over The lazy dog."));
-
-function Capitalize(str){
-
-      let capitalizedStr = "";
-  
-      let ascci_differences = "a".charCodeAt(0) - "A".charCodeAt(0);
-     
-      for(let i=0; i<str.length; i++){
-          capitalizedStr = ""
-          if((i==0 && str[i] >= "a" && str[i] <= "z") 
-          || (i>0 && str[i-1] == " " && str[i]>= "a" && str[i] <= "z")){
-              
-              capitalizedStr += String.fromCharCode(str[i].charCodeAt(0) - ascci_differences)
-              
-          } else {
-              capitalizedStr += str[i]
-          }
-      }
-      return capitalizedStr
-  
-  
+ let str = paragraph.toLowerCase();
+  // 1. create an empty string to store the capitalized answer.
+  let ans = "";
+  // 2. find the difference of ASCII values of the lowerCase and upperCase characters using charCodeAt(0)
+  let diff = "a".charCodeAt(0) - "A".charCodeAt(0);
+  // 3. loop through i to paragraph length
+  for (let i = 0; i < str.length; i++) {
+    // 3.1. check the first character of the each word.
+    //       => con-1 is to get the first words first letter  
+    //      (condition 1 => i == 0 && para[i] >= "a" && para[i]<= "z") ||
+    //       => con-2 is to get the other words in the str using empty spaces
+    //      (condition 2 => i > 0 && para[i - 1] == "" && para[i]>="a" && para[i]<="z")  
+    if (
+      (i == 0 && str[i] >= "a" && str[i] <= "z") ||
+      (i > 0 &&
+        str[i - 1] == " " &&
+        str[i] >= "a" &&
+        str[i] <= "z")
+    ) {
+      // 3.2. if any one condition is true, get the ascii value of the character - difference and from that value get character accordingly and concatenate with the capitalized answer string.
+      ans += String.fromCharCode(str[i].charCodeAt(0) - diff);
+    } else {
+      // 3.3. if none of the condition is true , return the exact string as concatenated
+      ans += str[i];
+    }
   }
-  let str = "capiTalIze tHe titLe"
-  console.log(Capitalize(str))
+  // 4. finally return capitalised answer string
+  return ans;
+}
+
+console.log(capitalise("capiTalIze tHe titLe"));
+
+// function Capitalize(str){
+
+//       let capitalizedStr = "";
+  
+//       let ascci_differences = "a".charCodeAt(0) - "A".charCodeAt(0);
+     
+//       for(let i=0; i<str.length; i++){
+//           capitalizedStr = ""
+//           if((i==0 && str[i] >= "a" && str[i] <= "z") 
+//           || (i>0 && str[i-1] == " " && str[i]>= "a" && str[i] <= "z")){
+              
+//               capitalizedStr += String.fromCharCode(str[i].charCodeAt(0) - ascci_differences)
+              
+//           } else {
+//               capitalizedStr += str[i]
+//           }
+//       }
+//       return capitalizedStr
+  
+  
+//   }
+//   let str = "capiTalIze tHe titLe"
+//   console.log(Capitalize(str))
   
 
 
 // INSTRUCTIONS:
 // 1. create an empty string to store the capitalized answer.
 // 2. find the difference of ASCII values of the lowerCase and upperCase characters using charCodeAt(0)
-// 3. loop through i to paragraph length
+// 3. loop through i to str length
   // 3.1. check the first character of the each word.
     //       => con-1 is to get the first words first letter  
     //      (condition 1 => i == 0 && para[i] >= "a" && para[i]<= "z") ||
